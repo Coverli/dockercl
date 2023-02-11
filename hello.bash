8cf7596a111aedd015986678953319296569e213
Coverli <2870118626@qq.com>
2023-02-11 18:53:58 +0800

    26

--- hello.bash
+++ hello.bash
@@ -1,13 +1,10 @@
 sudo touch Dockerfile
-sudo ls -a
 sudo echo FROM ubuntu:20.04 >> Dockerfile
 sudo echo COPY . /app >> Dockerfile
 sudo echo WORKDIR /app >> Dockerfile
 sudo echo RUN echo "start" >> Dockerfile
 sudo echo CMD echo "Hello World!" >> Dockerfile
 sudo echo EXPOSE 4444 >> Dockerfile
-sudo cat Dockerfile
-sudo pwd
 
 sudo docker image build -t helloworld .
 
