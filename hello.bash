56076a2506d1d98341c480083fc72d8b5479e8d3
Coverli <2870118626@qq.com>
2023-02-11 18:16:05 +0800

    19

--- hello.bash
+++ hello.bash
@@ -1,10 +1,11 @@
-echo FROM ubuntu:20.04 > Dockfile
-echo COPY . /app >> Dockfile
-echo WORKDIR /app >> Dockfile
-echo RUN echo "start" >> Dockfile
-echo CMD echo "Hello World!" >> Dockfile
-echo EXPOSE 4444 >> Dockfile
+sudo touch Dockfile
+sudo echo FROM ubuntu:20.04 > Dockfile
+sudo echo COPY . /app >> Dockfile
+sudo echo WORKDIR /app >> Dockfile
+sudo echo RUN echo "start" >> Dockfile
+sudo echo CMD echo "Hello World!" >> Dockfile
+sudo echo EXPOSE 4444 >> Dockfile
 
-docker image build -t helloworld .
+sudo docker image build -t helloworld .
 
-docker container run -p 8000:4444 -it helloworld
+sudo docker container run -p 8000:4444 -it helloworld
